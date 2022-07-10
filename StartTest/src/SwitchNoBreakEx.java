a422b7f06bdc404474f41ebb3dad3e697747212c
blackJJW <jjinwoo92@gmail.com>
2022-07-10 13:28:02 +0900

    switch

---
+++ StartTest/src/SwitchNoBreakEx.java
@@ -0,0 +1,22 @@
+
+public class SwitchNoBreakEx {
+
+	public static void main(String[] args) {
+		int time = (int)(Math.random() * 4) + 8; 
+			// 8<= ... <=11 사이의 정수 뽑기
+		System.out.println("[현재시간 : " + time + " 시]");
+		
+		switch(time) {
+			case 8:
+				System.out.println("출근");
+			case 9:
+				System.out.println("회의");
+			case 10:
+				System.out.println("업무");
+			default:
+				System.out.println("외근");
+		}
+
+	}
+
+}
