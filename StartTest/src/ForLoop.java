cfc4afdd29222e6b831f17badd6da16e7482bab7
blackJJW <jjinwoo92@gmail.com>
2022-07-10 17:20:24 +0900

    for 문

---
+++ StartTest/src/ForLoop.java
@@ -0,0 +1,16 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+				
+		for (i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println("i : " + i);
+		System.out.println("1 ~ 100까지의 합 : " + sum);
+
+	}
+
+}
