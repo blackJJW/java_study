fbf1d3bc5d8a48f2a6b5a631029b78a29b3d75be
blackJJW <jjinwoo92@gmail.com>
2022-07-12 03:21:49 +0900

    jvm stack

---
+++ StartTest/src/JVMStackEx.java
@@ -0,0 +1,18 @@
+
+public class JVMStackEx {
+
+	public static void main(String[] args) {
+		char v1 = 'A';
+		
+		if(v1 == 'A') {
+			int v2 = 100;
+			double v3 = 3.14;
+		}
+		
+		boolean v4 = true;
+		
+		
+
+	}
+
+}
