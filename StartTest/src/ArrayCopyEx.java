21014d667ecdfff05bb19c3cac0c128d6437dc1f
blackJJW <jjinwoo92@gmail.com>
2022-07-16 01:26:19 +0900

    20220716

---
+++ StartTest/src/ArrayCopyEx.java
@@ -0,0 +1,16 @@
+
+public class ArrayCopyEx {
+
+	public static void main(String[] args) {
+		String[] oldStrArray = {"Java", "Array", "Copy"};
+		String[] newStrArray = new String[5];
+		
+		System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+		
+		for(int i = 0; i < newStrArray.length; i++) {
+			System.out.print(newStrArray[i] + ", ");
+		}
+
+	}
+
+}
---
+++ StartTest/src/ArrayInArrayEx.java
@@ -0,0 +1,12 @@
+
+public class ArrayInArrayEx {
+
+	public static void main(String[] args) {
+		int[][] scores = {{95, 80}, {92, 96}};
+		
+		System.out.println(scores[0][0]);
+		System.out.println(scores[1][1]);
+
+	}
+
+}
