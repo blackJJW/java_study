d1d8005b99e93ee2bff7acaaa89e831df9b7026a
blackJJW <jjinwoo92@gmail.com>
2022-07-30 03:04:54 +0900

    2022

--- StartTest/src/com/test2/package2/C.java
+++ StartTest/src/com/test2/package2/C.java
@@ -1,9 +1,17 @@
 package com.test2.package2; // 패키지가 다름
 
-import com.test2.package1.*;
+import com.test2.package1.A;
 
 public class C {
-	A a; // A 클래스 접근 불가(컴파일 에러)
-	B b;
+	public C() {
+		A a = new A();
+		a.field1 = 1; // (o)
+		a.field2 = 1; // (x) default 접근 제한(컴파일 에러)
+		a.field3 = 1; // (x) private 접근 제한(컴파일 에러)
+		
+		a.method1(); // (o)
+		a.method2(); // (x)  default 접근 제한(컴파일 에러)
+		a.method3(); // (x)  private 접근 제한(컴파일 에러)
+	}
 
 }
