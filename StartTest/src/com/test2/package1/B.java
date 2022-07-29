ce1a64f6ebc7f33a4024802bb28b2a06942a9d95
blackJJW <jjinwoo92@gmail.com>
2022-07-30 03:04:43 +0900

    2022

--- StartTest/src/com/test2/package1/B.java
+++ StartTest/src/com/test2/package1/B.java
@@ -1,5 +1,14 @@
-package com.test2.package1;
+package com.test2.package1;// 패키지가 동일
 
 public class B {
-	A a; // A 클래스 접근 가능
+	public B() {
+		A a = new A();
+		a.field1 = 1; // (o)
+		a.field2 = 1; // (o)
+		a.field3 = 1; // (x) private 접근 제한(컴파일 에러)
+		
+		a.method1(); // (o)
+		a.method2(); // (o)
+		a.method3(); // (x)  private 접근 제한(컴파일 에러)
+	}
 }
