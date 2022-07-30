19d1184fa51b38842f730e3e7a6fe62ed99a5087
blackJJW <jjinwoo92@gmail.com>
2022-07-31 01:14:13 +0900

    20220731

---
+++ StartTest/src/getterAndSetter/CarEx.java
@@ -0,0 +1,23 @@
+package getterAndSetter;
+
+public class CarEx {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		
+		// 잘몫된 속도 변경
+		myCar.setSpeed(-50);
+		
+		System.out.println("현재 속도 : " + myCar.getSpeed());
+		
+		// 올바른 속도 변경
+		myCar.setSpeed(60);
+		
+		// 멈춤
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		System.out.println("현재 속도 : " + myCar.getSpeed());
+
+	}
+}
