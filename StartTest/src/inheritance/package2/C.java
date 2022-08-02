2c61cf2e31d8f1c87d7226f14f2a0d2afd463edf
blackJJW <jjinwoo92@gmail.com>
2022-08-03 02:11:01 +0900

    20220803

--- StartTest/src/inheritance/package2/C.java
+++ StartTest/src/inheritance/package2/C.java
@@ -1,5 +1,11 @@
 package inheritance.package2;
 
-public class C {
+import inheritance.package1.A;
 
+public class C {
+	public void method() {
+		A a = new A();     // (x)
+		a.field = "value"; // (x)
+		a.method();        // (x)
+	}
 }
