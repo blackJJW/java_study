2415a48f851ade1fce9a04cccf9365f36791978c
blackJJW <jjinwoo92@gmail.com>
2022-08-16 02:18:39 +0900

    2022

--- StartTest/src/interfacePrac/ChildInterface3.java
+++ StartTest/src/interfacePrac/ChildInterface3.java
@@ -1,5 +1,8 @@
 package interfacePrac;
 
-public interface ChildInterface3 {
+public interface ChildInterface3 extends ParentInterface{
 
+	@Override
+	public void method2(); // 추상 메소드로 재선언
+	public void method3();
 }
