a693eb46c6f5ebb4f6227219474d7a71f2cc23ed
blackJJW <jjinwoo92@gmail.com>
2022-09-17 21:17:33 +0900

    equals() 메소드

---
+++ StartTest/src/basicAPI/MemberEx.java
@@ -0,0 +1,24 @@
+package basicAPI;
+
+public class MemberEx {
+
+	public static void main(String[] args) {
+		Member obj1 = new Member("blue");
+		Member obj2 = new Member("blue");
+		Member obj3 = new Member("red");
+		
+		if(obj1.equals(obj2)) { // 매개값이 Member 타입이고 id 필드값도 동일하므로 true
+			System.out.println("obj1과 obj2는 동등");
+		} else {
+			System.out.println("obj1과 obj2는 다름");
+		}
+		
+		if(obj1.equals(obj3)) { // 매개값이 Member 타입이지만 id 필드값이 다르므로 false
+			System.out.println("obj1과 obj3은 동일");
+		} else {
+			System.out.println("obj1과 obj3은 다름");
+		}
+
+	}
+
+}
