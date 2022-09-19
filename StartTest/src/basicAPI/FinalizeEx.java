39bb500e1e431a811306e4e4ea9ba484f1e2fbdd
blackJJW <jjinwoo92@gmail.com>
2022-09-20 00:01:11 +0900

    finalize()

---
+++ StartTest/src/basicAPI/FinalizeEx.java
@@ -0,0 +1,17 @@
+package basicAPI;
+
+public class FinalizeEx {
+
+	public static void main(String[] args) {
+		Counter counter = null;
+		for(int i = 1; i <= 50; i++) {
+			counter = new Counter(i);
+			
+			counter = null; // Counter 객체를 garbage로 만듦
+			
+			System.gc(); // Garbage collector 요청
+		}
+
+	}
+
+}
