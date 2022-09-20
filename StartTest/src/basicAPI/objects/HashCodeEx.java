9497f970c52dae439be95f59b89e1792c19adf4c
blackJJW <jjinwoo92@gmail.com>
2022-09-21 01:03:14 +0900

    hashCode

---
+++ StartTest/src/basicAPI/objects/HashCodeEx.java
@@ -0,0 +1,29 @@
+package basicAPI.objects;
+
+import java.util.Objects;
+
+public class HashCodeEx {
+
+	public static void main(String[] args) {
+		Student s1 = new Student(1, "ABC");
+		Student s2 = new Student(1, "ABC");
+		System.out.println(s1.hashCode());
+		System.out.println( Objects.hashCode(s2) );
+
+	}
+	
+	static class Student{
+		int sno;
+		String name;
+		Student(int sno, String name){
+			this.sno = sno;
+			this.name = name;
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(sno, name);
+		}
+		
+	}
+
+}
