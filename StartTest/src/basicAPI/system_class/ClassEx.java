b17e202b5b7cdc967715fd8609324e8fe94156ad
blackJJW <jjinwoo92@gmail.com>
2022-09-23 01:01:28 +0900

    Class class

---
+++ StartTest/src/basicAPI/system_class/ClassEx.java
@@ -0,0 +1,26 @@
+package basicAPI.system_class;
+
+import interfacePrac.Car;
+
+public class ClassEx {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		Class clazz1 = car.getClass();
+		System.out.println(clazz1.getName());
+		System.out.println(clazz1.getSimpleName());
+		System.out.println(clazz1.getPackage().getName());
+		System.out.println();
+		
+		try {
+			Class clazz2 = Class.forName("interfacePrac.Car");
+			System.out.println(clazz2.getName());
+			System.out.println(clazz2.getSimpleName());
+			System.out.println(clazz2.getPackage().getName());
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
