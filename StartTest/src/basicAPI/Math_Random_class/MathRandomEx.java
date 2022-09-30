e5d76f77023a6df3c3bf915ebb77774678a13d72
blackJJW <jjinwoo92@gmail.com>
2022-10-01 01:18:50 +0900

    mathrandom

---
+++ StartTest/src/basicAPI/Math_Random_class/MathRandomEx.java
@@ -0,0 +1,11 @@
+package basicAPI.Math_Random_class;
+
+public class MathRandomEx {
+
+	public static void main(String[] args) {
+		int num = (int) (Math.random() + 6) + 1;
+		System.out.println("주사위 눈 : " + num);
+
+	}
+
+}
