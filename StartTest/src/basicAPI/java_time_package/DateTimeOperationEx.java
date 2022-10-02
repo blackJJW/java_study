566e219c14475a976a06ab0cc7a0ff5632c2ae9a
blackJJW <jjinwoo92@gmail.com>
2022-10-02 22:49:33 +0900

    시간 날짜

---
+++ StartTest/src/basicAPI/java_time_package/DateTimeOperationEx.java
@@ -0,0 +1,21 @@
+package basicAPI.java_time_package;
+
+import java.time.LocalDateTime;
+
+public class DateTimeOperationEx {
+
+	public static void main(String[] args) {
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("현재시 : " + now);
+		
+		LocalDateTime targetDateTime = now
+				.plusYears(1)
+				.minusMonths(2)
+				.plusDays(3)
+				.plusHours(4)
+				.minusMinutes(5)
+				.plusSeconds(6);
+		System.out.println("연산후 : " + targetDateTime);
+	}
+
+}
