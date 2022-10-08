a89c87802fbbda64d061dc9094d7d9c22c2e021d
blackJJW <jjinwoo92@gmail.com>
2022-10-09 00:25:39 +0900

    sleep()

---
+++ StartTest/src/multi_thread/SleepEx.java
@@ -0,0 +1,20 @@
+package multi_thread;
+
+import java.awt.Toolkit;
+
+public class SleepEx {
+
+	public static void main(String[] args) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for(int i = 0; i < 10; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(3000); // 3초 동안 메인 스레드를 일시 정지 상태로 만듦
+			}catch(InterruptedException e) {
+				
+			}
+		}
+
+	}
+
+}
