34f097e7505afba2ad827ac4eae80343cae1f61a
blackJJW <jjinwoo92@gmail.com>
2022-10-13 00:51:20 +0900

    daemon thread

---
+++ StartTest/src/multi_thread/A1/DaemonEx.java
@@ -0,0 +1,19 @@
+package multi_thread.A1;
+
+public class DaemonEx {
+
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setDaemon(true); // AutoSaveThread를 데몬 스레드로 만듦
+		autoSaveThread.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+		}
+		
+		System.out.println("메인 스레드 종료");
+
+	}
+
+}
