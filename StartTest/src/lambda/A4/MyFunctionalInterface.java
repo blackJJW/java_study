c9d3d474b89fed2372f23c8ed19f4871e88a7dbc
blackJJW <jjinwoo92@gmail.com>
2022-10-30 20:00:40 +0900

    클래스 멤버 사용

---
+++ StartTest/src/lambda/A4/MyFunctionalInterface.java
@@ -0,0 +1,5 @@
+package lambda.A4;
+
+public interface MyFunctionalInterface {
+	public void method();
+}
---
+++ StartTest/src/lambda/A4/UsingThis.java
@@ -0,0 +1,22 @@
+package lambda.A4;
+
+public class UsingThis {
+	public int outterField = 10;
+	
+	class Inner {
+		int innerField = 20;
+		
+		void method() {
+			// 람다식
+			MyFunctionalInterface fi = () -> {
+				System.out.println("outterField : " + outterField);
+				System.out.println("outterField : " + UsingThis.this.outterField + "\n");
+				                             // 바깥 객체의 참조를 얻기 위해서는 클래스명 this를 사용
+				
+				System.out.println("innerField : " + innerField);
+				System.out.println("innerField : " + this.innerField + "\n");
+				                            // 람다식 내부에서 this는 Inner 객체를 참조
+			};
+		}
+	}
+}
