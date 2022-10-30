163c1461629b7bb810f69bb0868ade064a918fb2
blackJJW <jjinwoo92@gmail.com>
2022-10-30 21:45:24 +0900

    로컬 변수 사용

---
+++ StartTest/src/lambda/A5/UsingLocalVariableEx.java
@@ -0,0 +1,9 @@
+package lambda.A5;
+
+public class UsingLocalVariableEx {
+
+	public static void main(String... args) {
+		UsingLocalVariable ulv = new UsingLocalVariable();
+		ulv.method(20);
+	}
+}
