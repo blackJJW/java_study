b36938fd81a57bc6231f39fbcfbc8eec396d130b
blackJJW <jjinwoo92@gmail.com>
2022-11-03 00:09:03 +0900

    Function 2

---
+++ StartTest/src/lambda/A7/FunctionEx2.java
@@ -0,0 +1,32 @@
+package lambda.A7;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+public class FunctionEx2 {
+	private static List<Student> list = Arrays.asList(
+			new Student("ABC", 90, 96),
+			new Student("DEF", 95, 93)
+			);
+	
+	public static double avg( ToIntFunction<Student> function ) {
+		int sum = 0;
+		for(Student student : list) {
+			sum += function.applyAsInt(student);
+			     // 람다식 실행
+		}
+		double avg = (double) sum / list.size();
+		return avg;
+	}
+
+	public static void main(String[] args) {
+		double englishAvg = avg( s -> s.getEnglishScore() );
+		System.out.println("영어 평균 점수 : " + englishAvg);
+		
+		double mathAvg = avg( s -> s.getMathScore() );
+		System.out.println("수학 평균 점수 : " + mathAvg);
+
+	}
+
+}
