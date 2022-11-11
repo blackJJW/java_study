74863b824deccc5db7cdb4ad3925dbd36ab643fb
blackJJW <jjinwoo92@gmail.com>
2022-11-11 16:34:09 +0900

    Arrays.asList()

---
+++ StartTest/src/collectionFramework/ArraysAsListEx.java
@@ -0,0 +1,22 @@
+package collectionFramework;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListEx {
+
+	public static void main(String[] args) {
+		List<String> list1 = Arrays.asList("A", "B", "C");
+		
+		for(String name : list1) {
+			System.out.println(name);
+		}
+		
+		List<Integer> list2 = Arrays.asList(1, 2, 3);
+		for(int value : list2) {
+			System.out.println(value);
+		}
+
+	}
+
+}
