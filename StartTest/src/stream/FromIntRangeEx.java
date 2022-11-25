8658f512ff3798d9e0170fd9e9c435b0fa7cc606
blackJJW <jjinwoo92@gmail.com>
2022-11-26 00:55:56 +0900

    20221126

---
+++ StartTest/src/stream/FromIntRangeEx.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.stream.IntStream;
+
+public class FromIntRangeEx {
+	
+	public static int sum;
+	
+	public static void main(String[] args) {
+		IntStream stream = IntStream.rangeClosed(1, 100);
+		stream.forEach(a -> sum += a);
+		System.out.println("총합 : " + sum);
+	}
+}
