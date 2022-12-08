a3a985ee98aa1fe9e3a5ab1c8196c98a7df6864c
blackJJW <jjinwoo92@gmail.com>
2022-12-08 23:59:23 +0900

    collect

---
+++ StartTest/src/stream/collect/MaleStudent.java
@@ -0,0 +1,32 @@
+package stream.collect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaleStudent {
+	private List<Student> list; // 요소를 저장할 컬렉션
+	
+	public MaleStudent() {
+		list = new ArrayList<Student>();
+		System.out.println("[ " + Thread.currentThread().getName() + " ] MaleStudent()");
+		                        // 생성자를 호출하는 스레드 이름
+	}
+	
+	public void accumulate(Student student) { // 요소를 수집하는 메소드
+		list.add(student);
+		System.out.println("[ " + Thread.currentThread().getName() + " ] accumulate()");
+                                // accumulate()를 호출하는 스레드 이름
+	}
+	
+	public void combine(MaleStudent other) { /* 두 MaleStudent를 결합하는 메소드
+	                                          * (병렬 처리 시에만 호출)
+	                                          * */
+		list.addAll(other.getList());
+		System.out.println("[ " + Thread.currentThread().getName() + " ] combine()");
+                               // combine()을 호출하는 스레드 이름
+	}
+	
+	public List<Student> getList(){ // 요소가 저장된 컬렉션을 리턴
+		return list;
+	}
+}
--- StartTest/src/stream/collect/ToListEx.java
+++ StartTest/src/stream/collect/ToListEx.java
@@ -1,10 +1,37 @@
 package stream.collect;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 public class ToListEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Student> totalList = Arrays.asList(
+				new Student("ABC", 10, Student.Sex.MALE),
+				new Student("DEF", 6, Student.Sex.FEMALE),
+				new Student("GHI", 10, Student.Sex.MALE),
+				new Student("JKL", 6, Student.Sex.FEMALE)
+		);
+		
+		// 남학생들만 묶어 List 생성
+		List<Student> maleList = totalList.stream()
+				.filter(s -> s.getSex() == Student.Sex.MALE)
+				.collect(Collectors.toList());
+		maleList.stream()
+			.forEach(s -> System.out.println(s.getName()));
+		
+		System.out.println();
+		
+		// 여학생들만 묶어 HashSet 생성
+		Set<Student> femaleSet = totalList.stream()
+				.filter(s -> s.getSex() == Student.Sex.FEMALE)
+				.collect(Collectors.toCollection(HashSet :: new));
+		femaleSet.stream()
+			.forEach(s -> System.out.println(s.getName()));
 
 	}
-
 }
