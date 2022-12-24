02e88212001c759c9a4850c6e50b2b14ec5226b9
blackJJW <jjinwoo92@gmail.com>
2022-12-25 00:21:27 +0900

    media control

--- third_javafx/src/mediaView/RootController.java
+++ third_javafx/src/mediaView/RootController.java
@@ -3,13 +3,20 @@ package mediaView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 public class RootController implements Initializable {
 	@FXML private MediaView mediaView;
@@ -17,6 +24,12 @@ public class RootController implements Initializable {
 	@FXML private Button btnPlay;
 	@FXML private Button btnPause;
 	@FXML private Button btnStop;
+	
+	// Slider & ProgressBar & ProgressIndicator
+	@FXML private Label labelTime;
+	@FXML private Slider sliderVolume;
+	@FXML private ProgressBar progressBar;
+	@FXML private ProgressIndicator progressIndicator;
 	
 	private boolean endOfMedia; // 재생 완료를 확인하는 플래그 필드
 	
@@ -79,5 +92,51 @@ public class RootController implements Initializable {
 		                            // 미디어 일시 정지
 		btnStop.setOnAction(event -> mediaPlayer.stop());
 		                            // 미디어 중지
+		
+		// 해당 상태가 되면 실행할 Runnable 설정
+		mediaPlayer.setOnReady(new Runnable() {
+			@Override
+			public void run() {
+				mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+					@Override
+					public void changed(ObservableValue<? extends Duration> observable,
+							            Duration oldValue, Duration newValue) {
+						double progress = mediaPlayer.getCurrentTime().toSeconds() / 
+								mediaPlayer.getTotalDuration().toSeconds();
+						progressBar.setProgress(progress);
+						progressIndicator.setProgress(progress);
+						labelTime.setText(
+								(int) mediaPlayer.getCurrentTime().toSeconds() + "/" +
+								(int) mediaPlayer.getTotalDuration().toSeconds() + " sec");
+					}
+				});
+				
+				btnPlay.setDisable(false);
+				btnPause.setDisable(true);
+				btnStop.setDisable(true);
+			}
+		});
+		
+		mediaPlayer.setOnEndOfMedia(() -> {
+			// 미디어 재생이 완료되었을 때 progress의 속성값을 1.0으로 설정
+			progressBar.setProgress(1.0);
+			progressIndicator.setProgress(1.0);
+			
+			endOfMedia = true;
+			btnPlay.setDisable(false);
+			btnPause.setDisable(true);
+			btnStop.setDisable(true);
+		});
+		
+		// 볼륨 설정
+		// Slider의 value 속성 감시
+		sliderVolume.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				mediaPlayer.setVolume(sliderVolume.getValue() / 100.0);
+			}
+		});
+		sliderVolume.setValue(50.0); // Slider 초기값
 	}
 }
