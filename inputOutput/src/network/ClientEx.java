628970ea573f75186496e823a33a985e07b116b7
blackJJW <jjinwoo92@gmail.com>
2023-01-21 16:34:16 +0900

    client server

--- inputOutput/src/network/ClientEx.java
+++ inputOutput/src/network/ClientEx.java
@@ -1,6 +1,8 @@
 package network;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -15,6 +17,29 @@ public class ClientEx {
 			// 연결 요청
 			socket.connect(new InetSocketAddress("localhost", 5001));
 			System.out.println(" [ 연결 성공 ] ");
+			
+			byte[] bytes = null;
+			String message = null;
+			
+			// 1. -------------------------------------------
+			OutputStream os = socket.getOutputStream();
+			message = "Hello Server";
+			bytes = message.getBytes("UTF-8");
+			os.write(bytes);
+			os.flush();
+			System.out.println(" [ 데이터 보내기 성공 ] ");
+			//-----------------------------------------------
+			
+			// 4. -------------------------------------------
+			InputStream is = socket.getInputStream();
+			bytes = new byte[100];
+			int readByteCount = is.read(bytes);
+			message = new String(bytes, 0, readByteCount, "UTF-8");
+			System.out.println(" [ 데이터 받기 성공 ] " + message);
+			
+			os.close();
+			is.close();
+			
 		} catch(Exception e) {}
 		
 		if(!socket.isClosed()) { // 연결되어 있을 경우
--- inputOutput/src/network/ServerEx.java
+++ inputOutput/src/network/ServerEx.java
@@ -1,6 +1,8 @@
 package network;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,6 +24,29 @@ public class ServerEx {
 				
 				InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
 				System.out.println("[ 연결 수락함 ]" + isa.getHostName());
+				
+				byte[] bytes = null;
+				String message = null;
+				
+				// 2.-------------------------------------------------------
+				InputStream is = socket.getInputStream();
+				bytes = new byte[100];
+				int readByteCount = is.read(bytes);
+				message = new String(bytes, 0, readByteCount, "UTF-8");
+				System.out.println(" [데이터 받기 성공]" + message);
+				//--------------------------------------------------
+				
+				// 3.-----------------------------------------------
+				OutputStream os = socket.getOutputStream();
+				message = "Hello Client";
+				bytes = message.getBytes("UTF-8");
+				os.write(bytes);
+				os.flush();
+				System.out.println(" [ 데이터 보내기 성공 ] ");
+				
+				is.close();
+				os.close();
+				socket.close();
 			}
 		} catch(Exception e) {}
 		
