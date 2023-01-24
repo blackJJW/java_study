a8ce068fd94ef9873a72d2f01166691b5f4c6217
blackJJW <jjinwoo92@gmail.com>
2023-01-24 14:07:07 +0900

    buffer size

---
+++ nio/src/buffer/BufferSizeEx.java
@@ -0,0 +1,14 @@
+package buffer;
+
+import java.nio.ByteBuffer;
+
+public class BufferSizeEx {
+
+	public static void main(String[] args) {
+		ByteBuffer directBuffer = ByteBuffer.allocateDirect(200 * 1024 * 1024);
+		System.out.println("다이렉트 버퍼가 생성됨");
+		
+		ByteBuffer nonDirectBuffer = ByteBuffer.allocate(200 * 1024 * 1024);
+		System.out.println("넌다이렉트 버퍼가 생성됨");
+	}
+}
