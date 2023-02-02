99d53235374b965acb26a7da9a45fe1f361c1b63
blackJJW <jjinwoo92@gmail.com>
2023-02-03 01:07:52 +0900

    UI, diplayText(), main()

--- nioBlockingChatting/src/application/ServerEx.java
+++ nioBlockingChatting/src/application/ServerEx.java
@@ -14,6 +14,12 @@ import java.util.concurrent.Executors;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class ServerEx extends Application {
 	ExecutorService executorService;
@@ -115,11 +121,6 @@ public class ServerEx extends Application {
 		} catch (Exception e) {}
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
 	// Client를 ServerEx의 내부 클래스로 선언
 	class Client {
 	  // 통신용 SocketChannel을 필드로 선언
@@ -208,6 +209,52 @@ public class ServerEx extends Application {
 		  // 스레드풀에서 처리
 		  executorService.submit(runnable);
 		}
+	}
+	
+	TextArea txtDisplay;
+	Button btnStartStop;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		root.setPrefSize(500, 300);
+		
+		txtDisplay = new TextArea();
+		txtDisplay.setEditable(false);
+		BorderPane.setMargin(txtDisplay, new Insets(0, 0, 2, 0));
+		root.setCenter(txtDisplay);
+		
+		btnStartStop = new Button("start");
+		btnStartStop.setPrefHeight(30);
+		btnStartStop.setMaxWidth(Double.MAX_VALUE);
+		
+		// start 버튼과 stop 버튼을 클릭했을 때 이벤트 처리
+		btnStartStop.setOnAction(e -> {
+			if(btnStartStop.getText().equals("start")) {
+				startServer();
+			} else if(btnStartStop.getText().equals("stop")) {
+				stopServer();
+			}
+		});
+		root.setBottom(btnStartStop);
+		
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("app.css").toString());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Server");
+		
+		// 윈도우 우측 상단 닫기 버튼을 클릭했을 때 이벤트 처리
+		primaryStage.setOnCloseRequest(event -> stopServer());
+		primaryStage.show();
+	}
+	
+	// 작업 스레드의 작업 처리 내용을 출력할 때 호출되는 메소드
+	void displayText(String text) {
+		txtDisplay.appendText(text + "\n");
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
